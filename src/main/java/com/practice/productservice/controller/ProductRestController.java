5db92932dfcaecc3387c9a59ae88a1665357f383
pankaj <pankaj@192.168.29.172>
2021-02-20 21:55:42 +0530

    Add feign client to call coupon microservice

--- src/main/java/com/practice/productservice/controller/ProductRestController.java
+++ src/main/java/com/practice/productservice/controller/ProductRestController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practice.productservice.model.Coupon;
 import com.practice.productservice.model.Product;
 import com.practice.productservice.repository.ProductRepo;
+import com.practice.productservice.restclient.CouponClient;
 
 @RestController
 @RequestMapping("/productapi")
@@ -16,8 +18,14 @@ public class ProductRestController {
 	@Autowired
 	private ProductRepo repo;
 
+	@Autowired
+	private CouponClient couponClient;
+
 	@PostMapping(value = "/products")
 	public Product create(@RequestBody Product product) {
+		String code = product.getCouponCode();
+		Coupon coupon = couponClient.getCoupon(code);
+		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return repo.save(product);
 
 	}
---
+++ src/main/java/com/practice/productservice/restclient/CouponClient.java
@@ -0,0 +1,15 @@
+package com.practice.productservice.restclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.practice.productservice.model.Coupon;
+
+@FeignClient("COUPON-SERVICE")
+public interface CouponClient {
+
+	@GetMapping("/couponapi/coupons/{code}")
+	Coupon getCoupon(@PathVariable("code") String code);
+
+}
