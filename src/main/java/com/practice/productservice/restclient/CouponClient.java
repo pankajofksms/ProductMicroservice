1920ee781a35eeaa4f27ee865877283a8e1e9474
pankaj <pankaj@192.168.29.172>
2021-02-22 18:31:08 +0530

    Remove client side load-balancing

--- src/main/java/com/practice/productservice/restclient/CouponClient.java
+++ src/main/java/com/practice/productservice/restclient/CouponClient.java
@@ -1,17 +1,19 @@
 package com.practice.productservice.restclient;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.practice.productservice.model.Coupon;
 
-@FeignClient("COUPON-SERVICE")
-@RibbonClient("COUPON-SERVICE")
+//@FeignClient("COUPON-SERVICE")
+@FeignClient("gateway-api-service")
+
+// Load balancer not needed as zuul provides server side load balancing
+//@LoadBalancerClient("COUPON-SERVICE")
 public interface CouponClient {
 
-	@GetMapping("/couponapi/coupons/{code}")
+	@GetMapping("/coupon-service/couponapi/coupons/{code}")
 	Coupon getCoupon(@PathVariable("code") String code);
 
 }
