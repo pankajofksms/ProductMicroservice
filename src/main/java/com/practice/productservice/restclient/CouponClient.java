5bd8a2fd927cd7d3156e00962d6afde51eb2c24c
pankaj <pankaj@192.168.29.172>
2021-02-22 09:09:07 +0530

    Add Ribbon config

--- src/main/java/com/practice/productservice/restclient/CouponClient.java
+++ src/main/java/com/practice/productservice/restclient/CouponClient.java
@@ -1,5 +1,6 @@
 package com.practice.productservice.restclient;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.practice.productservice.model.Coupon;
 
 @FeignClient("COUPON-SERVICE")
+@RibbonClient("COUPON-SERVICE")
 public interface CouponClient {
 
 	@GetMapping("/couponapi/coupons/{code}")
